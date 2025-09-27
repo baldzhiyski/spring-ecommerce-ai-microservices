edd5483c09f2a3d6938db402bc8ec6c29b985ae4
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-09-27 13:31:25 +0200

    Adding config

--- services/order/src/main/java/org/baldzhiyski/order/config/RabbitConfig.java
+++ services/order/src/main/java/org/baldzhiyski/order/config/RabbitConfig.java
@@ -1,25 +1,30 @@
 package org.baldzhiyski.order.config;
 
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.core.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
-
-    @Value("${app.mq.exchange}")
-    private String exchangeName;
-
     @Bean
-    public TopicExchange appEventsExchange() {
-        return new TopicExchange(exchangeName, true, false);
+    TopicExchange appEventsExchange(@Value("${app.mq.exchange}") String name) {
+        return new TopicExchange(name, true, false);
     }
 
     @Bean
-    public MessageConverter jacksonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    Queue paymentsQueue(@Value("${app.mq.queues.payments}") String name) {
+        return QueueBuilder.durable(name).build();
+    }
+
+    @Bean
+    Binding bindSucceeded(Queue paymentsQueue, TopicExchange appEventsExchange) {
+        return BindingBuilder.bind(paymentsQueue).to(appEventsExchange).with("payment.succeeded");
+    }
+
+    @Bean
+    Binding bindFailed(Queue paymentsQueue, TopicExchange appEventsExchange) {
+        return BindingBuilder.bind(paymentsQueue).to(appEventsExchange).with("payment.failed");
     }
 }
+
